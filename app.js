137d6855f13b4e02e38e0e2928640b874312772f
Albert Martinez <ialbertmartinez@outlook.com>
2021-09-03 10:51:24 -0700

    Express Generator REST API

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
