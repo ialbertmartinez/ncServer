abb7df2d8b05cbea5ff33699dad2935c455636f1
Albert Martinez <ialbertmartinez@outlook.com>
2021-09-12 01:40:54 -0700

    Passport

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -40,71 +43,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use( session({
   name: 'session-id',
   secret: '12345-67890-09876-54321',
-  saveUninitializedL: false,
+  saveUninitialized: false,
   resave: false,
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
-  console.log(req.session);
+    console.log(req.user);
 
-  if(!req.session.user) {
-    // console.log("request headers: ", req.headers);
-    const authHeader = req.headers.authorization;
-    
-    if (!authHeader) {
+    if (!req.user) {
         const err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
         err.status = 401;
         return next(err);
-    }
-
-    // console.log("authHeader", authHeader);
-    // console.log('Buffer.from("YWRtaW46cGFzc3dvcmQ=", "base64")\n', Buffer.from("YWRtaW46cGFzc3dvcmQ=", 'base64'));
-    // console.log('Buffer.from("YWRtaW46cGFzc3dvcmQ=", "base64").toString()', Buffer.from("YWRtaW46cGFzc3dvcmQ=", 'base64').toString());
-
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const user = auth[0];
-    const pass = auth[1];
-    // check username & password are valid
-    if (user === 'admin' && pass === 'password') {
-      req.session.user = 'admin';  
-      /*
-      res.cookie(arg1, arg2, arg3): create a new cookie for the authed user   
-        res.cookie('user', 'admin', {signed: true});
-        
-        'user'/arg1:        prop name/key for cookie. creates a 'user' property 
-                            on the signed cookie object
-        'admin'/arg2:       the value stored in the name property  (aka. user: 'admin')
-        {signed:true}/arg3: object with config values. set signed to true so express 
-                            can use the secret key from cookie-parser and make a signed cookie
-      */
-        return next(); // authorized
-    } else {
-        const err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');      
-        err.status = 401;
-        return next(err);
-    }
-  } // ./end of if(!req.signedCookies.user) conditional
-  else {
-    if(req.session.user === 'admin') {
-      return next(); // authorized
     } 
     else {
-        const err = new Error('You are not authenticated!');
-        err.status = 401;
-        return next(err);
+        return next();
     }
-  }
 }
 
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
