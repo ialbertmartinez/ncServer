0325dac78dd97421f28d2a12e92be0ae89309058
Albert Martinez <83342328+ialbertmartinez@users.noreply.github.com>
2021-09-20 04:30:43 -0700

    Delete config.js
    sensitive info

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	'secretKey': '12345-67890-09876-54321',
-	'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-	'facebook': {
-		'clientId': '1081511585924795',
-		'clientSecret': 'ca20e99e0b5a2f31cd1815685744b1a2'
-	}
-}
